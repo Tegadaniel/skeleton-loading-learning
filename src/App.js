47a07216fc035c90159f02812692d4b1a0b48dec
Oghenetega Philip <daniel.oghenetega@hts.com.ng>
2022-01-27 12:22:52 +0100

    "Another changes"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components';
+import Home from "./Components";
 
 function App() {
-  return (
-    <Home/>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,29 +1,92 @@
 import { Box, InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
-import React from "react";
+import { motion } from "framer-motion";
+import { fadeInUp, stagger } from "../motion";
+import React, { useState, useEffect } from "react";
+import { Api } from "../API/Api";
 import "../Global.css";
 
 export default function HomePage() {
+  let query;
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+    console.log("search here: ", search);
+  };
+
+  if (search === "") {
+    query = "Africa";
+  } else {
+    query = setSearch;
+  }
+
+  useEffect(() => {
+    Api(query).then((data) => {
+      console.log("What is data: ", data.results);
+      setData(data.results);
+    });
+  }, [query]);
   return (
-    <Box bg="#D3D3D3" w="100%" p={20}>
-      <InputGroup >
-        <InputLeftElement
-          marginLeft='140px'
-          top='11px'
-          pointerEvents="none"
-          children={<SearchIcon color="gray.300" />}
-          fontSize='lg'
-        />
-        <Input
-          className="noHover"
-          width="80%"
-          height="60px"
-          backgroundColor="white"
-          color="black"
-          type="tel"
-          placeholder="Search for photo"
-        />
-      </InputGroup>
-    </Box>
+    <main>
+      <Box bg="#D3D3D3" w="100%" p={20}>
+        <InputGroup>
+          <InputLeftElement
+            onChange={handleChange}
+            top="11px"
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+            fontSize="lg"
+          />
+          <Input
+            className="noHover"
+            height="60px"
+            backgroundColor="white"
+            color="black"
+            type="search"
+            placeholder="Search for photo"
+          />
+        </InputGroup>
+      </Box>
+      <section>
+        <motion.div
+          exit={{ opacity: 0 }}
+          initial="initial"
+          animate="animate"
+          className="grid-container"
+        >
+          <motion.div variants={stagger}>
+            {data.map((data, index) => (
+              <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                variants={fadeInUp}
+                key={index}
+                className="grid-item"
+              >
+                <motion.img
+                  initial={{ x: 60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  src={data.urls.small}
+                  alt={data.alt_description}
+                  className="img-collection"
+                />
+                <motion.div
+                  initial={{ x: 60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.6 }}
+                  className="overlay-text"
+                >
+                  <h6>{data.user.name}</h6>
+                  <small>{data.user.location}</small>
+                </motion.div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+      </section>
+    </main>
   );
 }
